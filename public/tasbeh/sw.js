910cb7225d385d91c86f681046a6d1b2184c432a
anugrah <anugrah@anugrahs-MacBook-Pro.local>
2026-04-17 14:57:00 +0700

    style(tasbeh): v2.8 - increase infinity svg size to 100% width and 450px max-width

--- public/tasbeh/sw.js
+++ public/tasbeh/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'fivetimes-cache-v7';
+const CACHE_NAME = 'fivetimes-cache-v8';
 const STATIC_ASSETS = [
   '/tasbeh/',
   '/tasbeh/index.html',
-  '/tasbeh/style.css?v=2.7',
-  '/tasbeh/app.js?v=2.7',
+  '/tasbeh/style.css?v=2.8',
+  '/tasbeh/app.js?v=2.8',
   '/tasbeh/icon.svg',
   '/tasbeh/icon-192.png',
   '/tasbeh/icon-512.png',
